8d9eddb33c738c9e3f490e7bfc0c749658067932
KailaTheeNgineer <kamcalpine717@gmail.com>
2024-04-30 12:55:04 -0400

    Finished all deposits screen

--- src/main/java/com/pluralsight/AccountingLedgerApp.java
+++ src/main/java/com/pluralsight/AccountingLedgerApp.java
@@ -4,17 +4,32 @@ package com.pluralsight;
 import java.io.*;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class AccountingLedgerApp {
     public static void main(String[] args) {
-        // While loop that initiates the ability to close out of the entire app
 
+        // Creating the array to access later in ledger
+        ArrayList<LedgerTransact> deposits = new ArrayList<>();
+        // Creating pre-saved object info for deposits
+        LedgerTransact id1 = new LedgerTransact("2024/04/28", "04:34:54", "Youtube", "Adsense", 2500);
+        LedgerTransact id2 = new LedgerTransact("2024/04/28", "04:35:35", "Gas money", "Cashapp", 150);
+        // Adding the objects to the array
+        deposits.add(id1);
+        deposits.add(id2);
+
+        ArrayList<LedgerTransact> debits = new ArrayList<LedgerTransact>();
+
+
+        // While loop that initiates the ability to close out of the entire app
         boolean homescreen = true;
         while (homescreen) {
             System.out.println("Account Ledger Home Screen\nTo begin, Select an option:\n");
             System.out.println("(D) Add Deposit\n(P) Make Debit Payment\n(L) Ledger\n(X) Exit");
             Scanner myScanner = new Scanner(System.in);
+
+
             String homeInput = myScanner.nextLine();
             if (homeInput.equalsIgnoreCase("D")) {
 
@@ -38,30 +53,35 @@ public class AccountingLedgerApp {
                             FileWriter depoWrite = new FileWriter("src/main/resources/transactions.csv", true);
                             BufferedWriter depoWriter = new BufferedWriter(depoWrite);
 
-                            // Allowing for system to write to a file and append is saving the info instead of overwriting for deposits.csv
-                            FileWriter depo2Write = new FileWriter("src/main/resources/deposits.csv", true);
-                            BufferedWriter depo2Writer = new BufferedWriter(depo2Write);
-
 
                             // Allowing for the date and time of the entry to be entered
-                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy/MM/dd");
                             DateTimeFormatter formatter33 = DateTimeFormatter.ofPattern("hh:mm:ss");
                             String datestamp22 = LocalDateTime.now().format(formatter);
                             String timestamp22 = LocalDateTime.now().format(formatter33);
+
 
                             // New entry for deposits if a valid entry with | is used for transactions.csv
                             depoWriter.write(datestamp22 + " | " + timestamp22 + " | " + input4Deposit);
                             depoWriter.newLine();
                             depoWriter.close();
 
-                            // New entry for deposits if a valid entry with | is used for deposits.csv
-                            depo2Writer.write(datestamp22 + " | " + timestamp22 + " | " + input4Deposit);
-                            depo2Writer.newLine();
-                            depo2Writer.close();
+                            // Parsing information in input4deposit to add to object
+                            String[] splitdeposit = input4Deposit.split("\\|");
+                            String description = splitdeposit[0];
+                            String vendor = splitdeposit[1];
+                            double amount = Double.parseDouble(splitdeposit[2]);
+
+                            // Creating an object out of the new information
+                            LedgerTransact inputDeposit = new LedgerTransact(datestamp22, timestamp22, description, vendor, amount);
+
+                            // Adding the object to the array
+                            deposits.add(inputDeposit);
 
 
                             System.out.println("\n\n\nSuccessfully Added Deposit Information!");
                             System.out.println("(R) Return Home\n(D) Enter another Deposit");
+
                             Scanner mouse1 = new Scanner(System.in);
                             String afterDeposit = mouse1.nextLine();
 
@@ -101,26 +121,21 @@ public class AccountingLedgerApp {
                     System.out.println("Enter (R) to return Home\n");
                     Scanner debitScanner = new Scanner(System.in);
                     System.out.println("Enter your Debit information in the following format:\n" +
-                            "[Description] | Vendor | Amount");
+                            "[Description] | Vendor | -Amount");
                     String input4Debit = debitScanner.nextLine();
 
                     // if statement for user to go back home from debit screen
                     if (input4Debit.equalsIgnoreCase("R")) {
                         break;
-                        // Note to self: Not the best way of collecting data, by detecting a "|" character
-                        // Could find a better way to represent this value
+
                     } else if (input4Debit.contains("|")) {
                         try {
                             // Allowing for system to write to a file and append is saving the info instead of overwrite for transactions.csv
                             FileWriter debitWrite = new FileWriter("src/main/resources/transactions.csv", true);
                             BufferedWriter debitWriter = new BufferedWriter(debitWrite);
 
-                            // Allowing for system to write to a file and append is saving the info instead of overwrite for debits.csv
-                            FileWriter debit2Write = new FileWriter("src/main/resources/debits.csv", true);
-                            BufferedWriter debit2Writer = new BufferedWriter(debit2Write);
-
                             // Allowing for the date and time of the entry to be entered
-                            DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+                            DateTimeFormatter formatter2 = DateTimeFormatter.ofPattern("yyyy/MM/dd");
                             DateTimeFormatter formatter3 = DateTimeFormatter.ofPattern("hh:mm:ss");
                             String datestamp2 = LocalDateTime.now().format(formatter2);
                             String timestamp2 = LocalDateTime.now().format(formatter3);
@@ -131,10 +146,6 @@ public class AccountingLedgerApp {
                             debitWriter.newLine();
                             debitWriter.close();
 
-                            // New entry for deposits if a valid entry with | is used for debits.csv
-                            debit2Writer.write(datestamp2 + " | " + timestamp2 + " | " + input4Debit);
-                            debit2Writer.newLine();
-                            debit2Writer.close();
 
                             System.out.println("\n\n\nSuccessfully Recorded Debit Information!");
                             System.out.println("(R) Return Home\n(P) Enter another Debit entry");
@@ -196,7 +207,8 @@ public class AccountingLedgerApp {
                                 while ((allTransactions = buffReader.readLine()) != null) {
                                     System.out.println(allTransactions);
 
-                                } buffReader.close();
+                                }
+                                buffReader.close();
 
                                 System.out.println("Enter any key to return to ledger screen");
                                 Scanner newScanner = new Scanner(System.in);
@@ -213,40 +225,28 @@ public class AccountingLedgerApp {
                             } catch (IOException e) {
                                 e.printStackTrace();
 
-                            }  break; // goes back to ledger screen if user enters (R)
+                            }
+                            break; // goes back to ledger screen if user enters (R)
                         }
                     } else if (ledger.equalsIgnoreCase("D")) {
 
                         // While statement to exit out of Deposit entry screen
                         boolean allDepositEntries = true;
-                        while (allDepositEntries) {try {
-
-                            // Reads deposits.csv and displays all entries to user
-                            FileReader readAll = new FileReader("src/main/resources/deposits.csv");
-                            BufferedReader buffReader = new BufferedReader(readAll);
-
-                            String allTransactions;
-
-                            while ((allTransactions = buffReader.readLine()) != null) {
-                                System.out.println(allTransactions);
-
-                            } buffReader.close();
-
-                            System.out.println("Enter any key to return to ledger screen");
-                            Scanner newScanner = new Scanner(System.in);
-                            String input = newScanner.nextLine();
-                            if (input.equalsIgnoreCase("R")) {
-                                break;
-
+                        while (allDepositEntries) {
+                            for (int i = 0; i < deposits.size(); i++) {
+                                System.out.print(deposits.get(i).getDate() + " | " + deposits.get(i).getTime()
+                                + " | " + deposits.get(i).getDescription() + " | " + deposits.get(i).getVendor() + " | ");
+                                System.out.printf("$%.2f\n", deposits.get(i).getAmount());
+                            }
+                            System.out.println("Enter any key to return to ledger screen: ");
+                            String newinput = myScanner.nextLine();
+                                if (newinput.equalsIgnoreCase("R")) {
+                                    break;
                             } else {
+
                                 break;
 
                             }
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-
-                        } break; // exits out of deposit screen back to ledger screen
 
 
 
@@ -258,39 +258,7 @@ public class AccountingLedgerApp {
                         boolean debitScreen = true;
                         // While statement to exit out of debit screen
                         while (debitScreen) {
-
-                            try {
-
-                            // Reads debits.csv and displays all entries to user
-                            FileReader read1All = new FileReader("src/main/resources/debits.csv");
-                            BufferedReader buffReader = new BufferedReader(read1All);
-
-                            String all1Transactions;
-
-                            while ((all1Transactions = buffReader.readLine()) != null) {
-                                System.out.println(all1Transactions);
-
-                            } buffReader.close();
-
-                            System.out.println("Enter any key to return to ledger screen");
-                            Scanner newScanner = new Scanner(System.in);
-                            String input = newScanner.nextLine();
-                            if (input.equalsIgnoreCase("R")) {
-                                break;
-
-                            } else {
-                                break;
-
-                            }
-
-                        } catch (IOException e) {
-                            e.printStackTrace();
-
                         }
-
-
-                        }
-
 
 
                     } else if (ledger.equalsIgnoreCase("R")) {
@@ -322,7 +290,7 @@ public class AccountingLedgerApp {
                 // if a valid option isn't chosen, System alerts the user, then runs homescreen()
                 // so they can try again
                 System.out.println("Please Select A Valid Option");
-                homescreen(true);
+                homescreen();
 
             }
             // used to exit application if the homescreen loop is broken via homeInput
@@ -337,14 +305,31 @@ public class AccountingLedgerApp {
 
         }
 
+
     // This method allows for user to exit application
     private static void exit() {
     }
 
     // This method returns to the homescreen by making the homescreen variable true
-    private static void homescreen(boolean b) {
+    private static void homescreen() {
     }
 
 
-}
+    // Method to print all deposit entries
+//    static void printAllDeposits(depo) {
+//        for (LedgerTransact d : depositList) {
+//            if (d.getAmount() >= 0) {
+//                System.out.println(d.getDate() + " | " + d.getTime() + " | " + d.getDescription() + " | "
+//                        + d.getVendor() + " | ");
+//                System.out.printf("$%.2f", d.getAmount());
+//                System.out.println("\n");
+//            }
+       // }
+
+  //  }
+
+    }
+
+
+
 
---
+++ src/main/java/com/pluralsight/LedgerTransact.java
@@ -0,0 +1,52 @@
+package com.pluralsight;
+
+public class LedgerTransact {
+    private String date;
+    private String time;
+    private String description;
+    private String vendor;
+    private double amount;
+
+    LedgerTransact() {
+        date = "0000-00-00";
+        time = "00:00:00";
+        description = "Unknown";
+        vendor = "Unknown";
+        amount = 0;
+
+
+    }
+
+    // Constructor method to access the data in the object
+    public LedgerTransact(String date, String time, String description, String vendor, double amount) {
+
+      this.date = date;
+      this.time = time;
+      this.description = description;
+      this.vendor = vendor;
+      this.amount = amount;
+
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getVendor() {
+        return vendor;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+
+}
